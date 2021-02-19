f9ef29e57782ef7da20d5c3be1d2300d61df32ba
OlegZadorogin <58337680+OlegZadorogin@users.noreply.github.com>
2021-02-19 11:13:26 +0300

    homework_dz3

--- frame/core.py
+++ frame/core.py
@@ -13,6 +13,8 @@ class Application:
     def parse_wsgi_input_data(self, data: bytes):
         result = {}
         if data:
+            # print('data_type', type(data))
+            # print(data)
             data_str = data.decode(encoding='utf-8')
             result = self.parse_input_data(data_str)
         return result
@@ -23,23 +25,18 @@ class Application:
         data = env['wsgi.input'].read(content_length) if content_length > 0 else b''
         return data
 
-    def __init__(self, urlpatterns: dict, front_controllers: list):
-        """
-        :param urlpatterns: словарь связок url: view
-        :param front_controllers: список front controllers
-        """
+    def __init__(self, urlpatterns, front_controllers):
         self.urlpatterns = urlpatterns
         self.front_controllers = front_controllers
 
     def __call__(self, env, start_answer):
-        # текущий url
+
         path = env['PATH_INFO']
 
         # добавление закрывающего слеша
         if not path.endswith('/'):
             path = f'{path}/'
 
-        # Получаем все данные запроса
         method = env['REQUEST_METHOD']
         data = self.get_wsgi_input_data(env)
         data = self.parse_wsgi_input_data(data)
@@ -48,23 +45,19 @@ class Application:
         request_params = self.parse_input_data(query_string)
 
         if path in self.urlpatterns:
-            # получаем view по url
+            # паттерн page controller
             view = self.urlpatterns[path]
             request = {}
-            # добавляем параметры запросов
+            # добавляем метод которым пришел запрос
             request['method'] = method
             request['data'] = data
             request['request_params'] = request_params
-            # добавляем в запрос данные из front controllers
             for controller in self.front_controllers:
+                # паттерн front controller
                 controller(request)
-            # вызываем view, получаем результат
             code, text = view(request)
-            # возвращаем заголовки
             start_answer(code, [('Content-Type', 'text/html')])
-            # возвращаем тело ответа
             return [text.encode('utf-8')]
         else:
-            # Если url нет в urlpatterns - то страница не найдена
             start_answer('404 NOT FOUND', [('Content-Type', 'text/html')])
             return [b"Not Found"]
